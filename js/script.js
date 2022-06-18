82a5f11e4803de2b09f2100ce573b7cab25cd467
Carnifex <shirov.ivan101@gmail.com>
2022-06-19 02:00:41 +0500

    fetch

--- js/script.js
+++ js/script.js
@@ -206,30 +206,32 @@ window.addEventListener('DOMContentLoaded', function() {
            statusMessage.classList.add('status');
            statusMessage.textContent = message.loading;
            form.appendChild(statusMessage);
-       
-           const request = new XMLHttpRequest();
-           request.open('POST', 'server.php');
-           request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    
+
            const formData = new FormData(form);
 
            const object = {};
            formData.forEach(function(value, key){
                object[key] = value;
            });
-           const json = JSON.stringify(object);
 
-           request.send(json);
-
-           request.addEventListener('load', () => {
-               if (request.status === 200) {
-                   console.log(request.response);
+           fetch('server.php', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json; charset=utf-8'
+            },
+            body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                   console.log(data);
                    showThanksModal(message.success);
-                   form.reset();
                    statusMessage.remove();
-               } else {
+            }).catch(() => {
                 showThanksModal(message.failure);
-               }
-           });
+            }).finally(() => {
+                form.reset();
+            })
        });
    }
 

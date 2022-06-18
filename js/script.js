122499ad82fec8a0f0c792183415cb77418937c3
Carnifex <shirov.ivan101@gmail.com>
2022-06-19 03:04:01 +0500

    jsonserver

--- js/script.js
+++ js/script.js
@@ -258,4 +258,8 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }, 4000)
    }
+
+   fetch('db.json')
+         .then(data => data.json())
+         .then(res => console.log(res))
 });

48542acd97ed4704285e17f427c0d92e53850cb5
Carnifex <shirov.ivan101@gmail.com>
2022-06-19 20:00:54 +0500

    webpack

---
+++ js/modules/cards.js
@@ -0,0 +1,55 @@
+function cards() {
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.transfer = 27;
+            this.parant = document.querySelector(parentSelector)
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = +this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+            </div>
+            `;
+            this.parant.append(element); 
+        }
+    }
+
+
+    const getResource = async (url) => {
+        const res = await fetch(url);
+        
+        if(!res.ok) {
+            throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        }
+
+        return await res.json();
+       };
+
+    getResource('http://localhost:3000/menu')
+       .then(data => {
+        data.forEach(({img, altimg, title, descr, price}) => {
+            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+        });
+       });
+}
+
+module.exports = cards;
